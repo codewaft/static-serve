efad68aeaabdb6502182882e2f65bc30136d182f
renjithspace <renjithrajeevvk@gmail.com>
2020-11-24 13:23:30 +0530

    Use static directory serve

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const route = (process.env.SPA === 'true') ? '*' : '/'
 
-app.get(route, (req, res) => {
-  res.sendFile(path.resolve(__dirname, process.env.ROOT_PATH))
-})
+app.use(express.static(process.env.ROOT_DIRECTORY))
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`)
