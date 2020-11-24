9ab0d075240a5fb65ffce7c7031d88275a7f4475
renjithspace <renjithrajeevvk@gmail.com>
2020-11-24 15:54:43 +0530

    Add spa routing

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+const routes = (process.env.SPA === 'true') ? '*' : ''
+const rootPath = `${process.env.ROOT_DIRECTORY}/index.html`
 
 app.use(express.static(process.env.ROOT_DIRECTORY))
+
+app.get(routes, function (request, response) {
+  response.sendFile(path.resolve(__dirname, rootPath))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`)
